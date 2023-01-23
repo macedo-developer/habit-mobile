6073f0353b773b88b78741dbd08370914b4ce40f
Renata Macedo <macedorenata.developer@gmail.com>
2023-01-22 23:33:34 -0300

    changing home and component habit day screens

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
-import { HabitDay, DAY_SIZE } from "./HabitDay";
-import { Header } from "./Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Header } from "../components/Header";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const dateFromYearStart = generateDatesFromYearBeginning();
@@ -12,6 +13,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - dateFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -33,7 +36,12 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {dateFromYearStart.map((date) => {
-            return <HabitDay key={date.toString()} />;
+            return (
+              <HabitDay
+                key={date.toString()}
+                onPress={() => navigate("habit", { date: date.toISOString() })}
+              />
+            );
           })}
 
           {amountOfDaysToFill > 0 &&
